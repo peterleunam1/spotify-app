9cf581c2066c3a093d63e12ff3a4d18072d1084e
peterleunan1 <arpm081001@gmail.com>
2025-02-24 19:10:38 -0500

    chore(global): :wrench: add configuration to align with latest ESLint documentation

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,7 +4,7 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [AppComponent]
     }).compileComponents();
   });
 
@@ -14,7 +14,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'spotify-app' title`, () => {
+  it('should have the \'spotify-app\' title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('spotify-app');
