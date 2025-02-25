c616b65bcc824a26de74205d551b34ab7cb06a6f
peterleunan1 <arpm081001@gmail.com>
2025-02-24 22:06:15 -0500

    feat(global): :lock: add route protection via guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { authGuard } from './guards/auth.guard';
+import { SignInComponent } from './pages/sign-in/sign-in.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'sign-in', component: SignInComponent },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    canActivate: [authGuard]
+  },
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponent }
+];
---
+++ src/app/pages/sign-in/sign-in.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-in',
+  imports: [],
+  templateUrl: './sign-in.component.html',
+  styleUrl: './sign-in.component.css'
+})
+export class SignInComponent {
+   login() {
+    localStorage.setItem('token-app', 'fdnfkojnknkfnfjknbnbnvb');
+    console.log('login');
+   }
+}
