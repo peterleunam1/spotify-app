0f99db60e59b26df353b20ce9a675e28d87138a5
peterleunam1 <arpm081001@gmail.com>
2025-02-26 14:39:26 -0500

    feat(global): :sparkles: add go back and go forward functionality

--- src/app/presentation/components/molecules/navbar/navbar.component.ts
+++ src/app/presentation/components/molecules/navbar/navbar.component.ts
@@ -1,12 +1,25 @@
-import { Component, EventEmitter, inject, Input, Output, signal } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  inject,
+  Input,
+  Output,
+  signal
+} from '@angular/core';
 import { Router } from '@angular/router';
 import { InputComponent } from '../../atoms/input/input.component';
-import { FormControl, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
+import {
+  FormControl,
+  ReactiveFormsModule,
+  ValidationErrors,
+  Validators
+} from '@angular/forms';
 import { debounceTime, distinctUntilChanged, lastValueFrom } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { ProfileModel } from '../../../../domain/profile/profile.entity';
 import { ProfileUseCase } from '../../../../application/profile/profile.use-case';
 import { TokenUseCase } from '../../../../application/token/token.use-case';
+import { PUBLIC_ROUTES } from '../../../constants/routes-storage.constant';
 
 @Component({
   selector: 'app-navbar',
@@ -21,38 +34,45 @@ export class NavbarComponent {
   private profileUseCase = inject(ProfileUseCase);
   private tokenUseCase = inject(TokenUseCase);
   private router = inject(Router);
-    profile: ProfileModel = {} as ProfileModel;
-    isLoading = false;
-    isDropdownOpen = false;
-  
-    async getProfile() {
-      try {
-        this.isLoading = true;
-        const result = await lastValueFrom(this.profileUseCase.getProfile());
-        this.profile = result;
-      } catch (error) {
-        console.error(error);
-      } finally {
-        this.isLoading = false;
-      }
-    }
-  
-    toggleDropdown() {
-      this.isDropdownOpen = !this.isDropdownOpen;
-    }
-  
-    logout() {
-      this.tokenUseCase.removeToken();
-      this.router.navigate(['/sign-in']);
-    }
-  
-    ngOnInit() {
-      this.getProfile();
-    }
+  profile: ProfileModel = {} as ProfileModel;
+  isLoading = false;
+  isDropdownOpen = false;
 
+  async getProfile() {
+    try {
+      this.isLoading = true;
+      const result = await lastValueFrom(this.profileUseCase.getProfile());
+      this.profile = result;
+    } catch (error) {
+      console.error(error);
+    } finally {
+      this.isLoading = false;
+    }
+  }
+
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
+
+  logout() {
+    this.tokenUseCase.removeToken();
+    this.router.navigate([PUBLIC_ROUTES.SIGN_IN]);
+  }
+
+  ngOnInit() {
+    this.getProfile();
+  }
+
+  goBack() {
+    window.history.back();
+  }
+
+  goForward() {
+    window.history.forward();
+  }
+  
   updateName(newValue: string) {
     this.keyword = newValue;
     this.keywordChange.emit(newValue);
   }
-
 }
