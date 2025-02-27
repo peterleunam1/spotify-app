1bea6a8ac7a6e50af9c84b404d4c658af3bc1b41
peterleunam1 <arpm081001@gmail.com>
2025-02-26 20:15:18 -0500

    fix: :zap: solve issue with env

---
+++ config.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+const successColor = '\x1b[32m%s\x1b[0m';
+const checkSign = '\u{2705}';
+const dotenv = require('dotenv').config({ path: 'src/.env' });
+
+const envFile = `export const environment = {
+    CLIENT_ID: '${process.env.CLIENT_ID}',
+    CLIENT_SECRET: '${process.env.CLIENT_SECRET}',
+    REDIRECT_URI: '${process.env.REDIRECT_URI}'
+};
+`;
+const targetPath = path.join(
+  __dirname,
+  './src/environments/environment.development.ts'
+);
+fs.writeFile(targetPath, envFile, err => {
+  if (err) {
+    console.error(err);
+    throw err;
+  } else {
+    console.log(
+      successColor,
+      `${checkSign} Successfully generated environment.development.ts`
+    );
+  }
+});
