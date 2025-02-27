17845fa44f4957e5d6f82872ff113bc2bf96556e
peterleunam1 <arpm081001@gmail.com>
2025-02-26 21:58:54 -0500

    build(global): :rocket: change angular configuration

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-require('dotenv').config();
+require('dotenv').config({path: 'src/.env'});
 
 const successColor = '\x1b[32m%s\x1b[0m';
 const checkSign = '\u{2705}';
@@ -11,6 +11,7 @@ const envFile = `export const environment = {
     REDIRECT_URI: '${process.env.REDIRECT_URI || ''}'
 };
 `;
+console.log(envFile);
 
 const targetPath = path.join(
   __dirname,
