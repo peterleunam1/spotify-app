c7eb4dd574a56509809161bab749ded9fb158f78
peterleunam1 <arpm081001@gmail.com>
2025-02-26 21:12:08 -0500

    refactor(global): :rocket: change script to set vars

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 const fs = require('fs');
 const path = require('path');
-require('dotenv').config();
+require('dotenv').config({ path: 'src/.env' });;
 
 const successColor = '\x1b[32m%s\x1b[0m';
 const checkSign = '\u{2705}';
 
 const envFile = `export const environment = {
-    CLIENT_ID: '${process.env.CLIENT_ID || ''}',
-    CLIENT_SECRET: '${process.env.CLIENT_SECRET || ''}',
+    CLIENT_ID: ${process.env.CLIENT_ID || ''}
+    CLIENT_SECRET: ${process.env.CLIENT_SECRET || ''}
     REDIRECT_URI: '${process.env.REDIRECT_URI || ''}'
 };
 `;
